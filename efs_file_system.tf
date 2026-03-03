bddd455b5f18409480cb20aa899fc1b5ad6557df
eblee9o <hello.eblee@gmail.com>
2026-03-03 06:57:38 +0000

    rm efs resources

--- efs_file_system.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_efs_file_system" "efs-file-system" {
-
-  encrypted                       = "false"
-  performance_mode                = "generalPurpose"
-  provisioned_throughput_in_mibps = "0"
-  throughput_mode                 = "bursting"
-}
-
